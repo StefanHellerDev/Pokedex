ce0fa774bb914e632b47226dcab07936d05d092c
Stefan Heller <S_Heller@gmx.de>
2025-09-25 21:42:43 +0200

    working on fetching the data

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 const BASE_URL = "https://pokeapi.co/api/v2/";
 let allPkmn = [];
 let offset = 0;
-let limit = 6;
-let pkmnTypes = [];
+let limit = 2;
+let singlePkmns = [];
 let content = document.getElementById("content");
 
 async function init() {
-  allPkmn = await loadPkmnOriginal("pokemon/", offset, limit);
-  for (let index = 0; index < allPkmn.results.length; index++) {
+  allPkmn = await loadPkmn("pokemon/", offset, limit);
+  console.log(allPkmn);
+  
+
+  for (let index = 0; index < allPkmn.length; index++) {
+    let pokemon = await loadSinglePokemons(index);
+    singlePkmns.push(pokemon);
+  }
+  
+  for (let index = 0; index < allPkmn.length; index++) {
     showTilesTemplate(index);
   }
 }
 
-async function loadPkmnOriginal(ext, offset, limit) {
+async function loadPkmn(ext, offset, limit) {
   let response = await fetch(BASE_URL + ext + "?offset=" + offset + "&limit=" + limit);
   response = await response.json();
+  return response.results;
+}
+
+async function loadSinglePokemons(index) {
+  let response = await fetch(allPkmn[index].url);
+  response = await response.json();
+  console.log(response);
   return response;
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,7 +1,7 @@
 function showTilesTemplate(index) {
     content.innerHTML +=
     `<div class="grow" onclick="switchOnOverlay(0)">
-    ${allPkmn.results[index].name.toUpperCase()}<br>
+    ${allPkmn[index].name.toUpperCase()}<br>
     type 1<br>
     type 2 -- bild
     </div>`;
