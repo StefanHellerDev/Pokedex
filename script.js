67e84b54f2e85d4ae63e157d9072d939b72281b7
Stefan Heller <S_Heller@gmx.de>
2025-09-26 17:35:52 +0200

    working on creating the tiles

--- script.js
+++ script.js
@@ -6,21 +6,19 @@ let singlePkmns = [];
 let content = document.getElementById("content");
 
 async function init() {
-  allPkmn = await loadPkmn("pokemon/", offset, limit);
-  console.log(allPkmn);
-  
+  allPkmn = await loadAllPkmn("pokemon/", offset, limit);
 
   for (let index = 0; index < allPkmn.length; index++) {
-    let pokemon = await loadSinglePokemons(index);
+    const pokemon = await loadSinglePokemons(index);
     singlePkmns.push(pokemon);
   }
-  
+
   for (let index = 0; index < allPkmn.length; index++) {
     showTilesTemplate(index);
   }
 }
 
-async function loadPkmn(ext, offset, limit) {
+async function loadAllPkmn(ext, offset, limit) {
   let response = await fetch(BASE_URL + ext + "?offset=" + offset + "&limit=" + limit);
   response = await response.json();
   return response.results;
@@ -29,6 +27,5 @@ async function loadPkmn(ext, offset, limit) {
 async function loadSinglePokemons(index) {
   let response = await fetch(allPkmn[index].url);
   response = await response.json();
-  console.log(response);
   return response;
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,8 +1,14 @@
 function showTilesTemplate(index) {
-    content.innerHTML +=
-    `<div class="grow" onclick="switchOnOverlay(0)">
+  let pkmnName = singlePkmns[index].name;
+  let pkmnId = singlePkmns[index].id;
+  let pkmnImage = singlePkmns[index].sprites.other["official-artwork"].front_default;
+  let backgroundType = singlePkmns[index].types[0].type.name;
+  let pkmnTypes = singlePkmns[index].types;
+  let types = pkmnTypes.map((t) => t.type.name);
+
+  content.innerHTML += `<div class="grow" onclick="switchOnOverlay(0)">
+    <img class="${backgroundType}" src="${pkmnImage}" alt="Image of ${pkmnName}">
     ${allPkmn[index].name.toUpperCase()}<br>
-    type 1<br>
-    type 2 -- bild
+    ${types.map((type) => `<span class="type ${type}">${type}</span>`).join("")}    
     </div>`;
 }
