ab6319173a03359d710b188164480cf478a7980e
Stefan Heller <S_Heller@gmx.de>
2025-10-02 19:49:42 +0200

    implemented the loader animation

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 const BASE_URL = "https://pokeapi.co/api/v2/";
 let allPkmn = [];
 let offset = 0;
-let limit = 10;
+let limit = 5;
 let singlePkmns = [];
 let content = document.getElementById("pkmn-content");
 
 async function init() {
-  allPkmn = await loadAllPkmn("pokemon/", offset, limit);
-  for (let index = 0; index < allPkmn.length; index++) {
-    const pokemon = await loadSinglePokemons(index);
-    singlePkmns.push(pokemon);
-  }
-  for (let index = 0; index < allPkmn.length; index++) {
-    showTilesTemplate(index);
-  }
+  loaderOn();
+  allPkmn = await fetchPkmn("pokemon/", 0, 10000);
+  await loadPkmn();
 }
 
-async function loadAllPkmn(ext, offset, limit) {
+async function loadPkmn() {
+  loaderOn();
+  for (let index = 0; index < limit; index++) {
+    const pokemon = await loadSinglePokemons(index + offset);
+    singlePkmns.push(pokemon);
+  }
+  for (let index = 0; index < limit; index++) {
+    showTilesTemplate(index + offset);
+  }
+  offset = offset + limit;
+  setTimeout(loaderOff, 1);
+  return;
+}
+
+async function fetchPkmn(ext, offset, limit) {
   let response = await fetch(BASE_URL + ext + "?offset=" + offset + "&limit=" + limit);
   response = await response.json();
   return response.results;
@@ -26,4 +35,14 @@ async function loadSinglePokemons(index) {
   let response = await fetch(allPkmn[index].url);
   response = await response.json();
   return response;
+}
+
+function loaderOn() {
+  let overlayRef = document.getElementById("loader-overlay");
+  overlayRef.classList.remove("d_none");
+}
+
+function loaderOff() {
+  let overlayRef = document.getElementById("loader-overlay");
+  overlayRef.classList.add("d_none");
 }
